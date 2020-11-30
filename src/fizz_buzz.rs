4f0061b598066054e456679e916d27f041e80ed7
Swann <swann@devloup.dev>
2020-11-30 23:07:39 +0100

    test match

--- src/fizz_buzz.rs
+++ src/fizz_buzz.rs
@@ -1,4 +1,11 @@
 pub fn fizz_buzz(n: i64) -> String {
+  match (n % 5, n % 3) {
+    (0, 0) => "FIZZ BUZZ".to_string(),
+    (0, _) => "BUZZ".to_string(),
+    (_, 0) => "FIZZ".to_string(),
+    _ => n.to_string(),
+  }
+
   if n % 15 == 0 {
     return "FIZZ BUZZ".to_string();
   }
